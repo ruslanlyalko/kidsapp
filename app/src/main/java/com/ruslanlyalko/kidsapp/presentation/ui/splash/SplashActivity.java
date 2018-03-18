394856612ac0da926f680fc0c16e4875e81dc5c1
DevRL1 <ruslan.lyalko2@gmail.com>
2018-03-18 21:30:59 +0200

    splash animation

--- app/src/main/java/com/ruslanlyalko/kidsapp/presentation/ui/splash/SplashActivity.java
+++ app/src/main/java/com/ruslanlyalko/kidsapp/presentation/ui/splash/SplashActivity.java
@@ -2,31 +2,66 @@ package com.ruslanlyalko.kidsapp.presentation.ui.splash;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.ruslanlyalko.kidsapp.presentation.service.MyFirebaseMessagingService;
-import com.ruslanlyalko.kidsapp.presentation.ui.main.MainActivity;
+import com.ruslanlyalko.kidsapp.R;
+import com.ruslanlyalko.kidsapp.presentation.base.BaseActivity;
 import com.ruslanlyalko.kidsapp.presentation.ui.login.LoginActivity;
+import com.ruslanlyalko.kidsapp.presentation.ui.main.MainActivity;
+
+import butterknife.BindView;
 
 /**
  * Created by Ruslan Lyalko
  * on 11.11.2017.
  */
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends BaseActivity {
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (FirebaseAuth.getInstance().getCurrentUser() == null)
-            startActivity(LoginActivity.getLaunchIntent(this));
-        else
-            startActivity(MainActivity.getLaunchIntent(this));
-        finish();
-    }
+    @BindView(R.id.text_app_name) TextView mTextAppName;
+    //@BindView(R.id.image_logo) ImageView mImageLogo;
 
     public static Intent getLaunchIntent(final Context context) {
         return new Intent(context, SplashActivity.class);
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    protected void setupView() {
+        Animation loadAnimation = AnimationUtils.loadAnimation(this, R.anim.splash);
+        mTextAppName.startAnimation(loadAnimation);
+        loadAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(final Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(final Animation animation) {
+                startMainActivity();
+            }
+
+            @Override
+            public void onAnimationRepeat(final Animation animation) {
+            }
+        });
+    }
+
+    private void startMainActivity() {
+        Intent intent;
+        if (FirebaseAuth.getInstance().getCurrentUser() == null)
+            intent = LoginActivity.getLaunchIntent(this);
+        else
+            intent = MainActivity.getLaunchIntent(this);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+        finish();
+        overridePendingTransition(R.anim.nothing, R.anim.nothing);
     }
 }
